b83484706487afb66dfe64ec5382d9e5438c6c21
Parama Danoesubroto <parama@answersengine.com>
2018-08-26 18:34:34 +0300

    added cli command to resume, cancel, and pause a job

--- lib/answersengine/cli/scraper_job.rb
+++ lib/answersengine/cli/scraper_job.rb
@@ -18,6 +18,37 @@ module AnswersEngine
         client = Client::ScraperJob.new(options)
         puts "#{client.all(scraper_id)}"
       end
+
+
+      desc "cancel <job_id>", "cancels a job"
+      option :page, :aliases => :p
+      long_desc <<-LONGDESC
+        Cancels a job
+      LONGDESC
+      def cancel(job_id)
+        client = Client::Job.new(options)
+        puts "#{client.cancel(job_id)}"
+      end
+
+      desc "resume <job_id>", "resumes a job"
+      option :page, :aliases => :p
+      long_desc <<-LONGDESC
+        Resumes a job
+      LONGDESC
+      def resume(job_id)
+        client = Client::Job.new(options)
+        puts "#{client.resume(job_id)}"
+      end
+
+      desc "pause <job_id>", "pauses a job"
+      option :page, :aliases => :p
+      long_desc <<-LONGDESC
+        pauses a job
+      LONGDESC
+      def pause(job_id)
+        client = Client::Job.new(options)
+        puts "#{client.pause(job_id)}"
+      end
       
     end
   end
--- lib/answersengine/client/job.rb
+++ lib/answersengine/client/job.rb
@@ -4,6 +4,32 @@ module AnswersEngine
       def all(opts={})
         self.class.get("/jobs", @options)
       end
+
+      def update(job_id, opts={})
+        body = {}
+
+        body[:status] = opts[:status] if opts[:status]
+        @options.merge!({body: body.to_json})
+
+        self.class.put("/jobs/#{job_id}", @options)
+      end
+
+      def cancel(job_id, opts={})
+        opts[:status] = 'cancelled'
+        update(job_id, opts)
+      end
+
+      def resume(job_id, opts={})
+        opts[:status] = 'active'
+        update(job_id, opts)
+      end
+
+      def pause(job_id, opts={})
+        opts[:status] = 'paused'
+        update(job_id, opts)
+      end
+
+
     end
 
   end
