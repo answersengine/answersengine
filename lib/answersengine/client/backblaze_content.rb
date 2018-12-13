c7b292a697ce047416fb14647fa440c40098a6b3
Parama Danoesubroto <parama@answersengine.com>
2018-12-13 16:20:31 -0500

    FETCH-42 fixing bug of httparty preparsing gzip as json.

--- lib/answersengine/client/backblaze_content.rb
+++ lib/answersengine/client/backblaze_content.rb
@@ -7,7 +7,7 @@ module AnswersEngine
       include HTTParty    
       
       def get_content(url)
-        self.class.get(url)
+        self.class.get(url, format: :plain)
       end
 
       def get_gunzipped_content(url)
