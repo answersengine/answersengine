898ca4679f1c3cfd941580b93cfebd69fb941b59
Parama Danoesubroto <parama@answersengine.com>
2019-03-23 02:57:45 -0400

    Made the scraper client to allow for variants to fields during update and create

--- lib/answersengine/client/scraper.rb
+++ lib/answersengine/client/scraper.rb
@@ -11,15 +11,14 @@ module AnswersEngine
       end
 
       def create(scraper_name, git_repository, opts={})
-        body = {
-            name: scraper_name,
-            git_repository: git_repository,
-            git_branch: opts[:branch] ? opts[:branch] : "master"}
-
+        body = {}
+        body[:name] = scraper_name
+        body[:git_repository] = git_repository,
+        body[:git_branch] = opts[:branch] || opts[:git_branch] || "master" if opts[:branch] || opts[:git_branch]
         body[:freshness_type] = opts[:freshness_type] if opts[:freshness_type]
         body[:force_fetch] = opts[:force_fetch] if opts[:force_fetch]
-        body[:standard_worker_count] = opts[:workers] if opts[:workers]
-        body[:browser_worker_count] = opts[:browsers] if opts[:browsers]
+        body[:standard_worker_count] = opts[:workers] || opts[:standard_worker_count] if opts[:workers] || opts[:standard_worker_count]
+        body[:browser_worker_count] = opts[:browsers] || opts[:browser_worker_count] if opts[:browsers] || opts[:browser_worker_count]
         body[:proxy_type] = opts[:proxy_type] if opts[:proxy_type]
         body[:disable_scheduler] = opts[:disable_scheduler] if opts[:disable_scheduler]
         body[:cancel_current_job] = opts[:cancel_current_job] if opts[:cancel_current_job]
@@ -31,17 +30,16 @@ module AnswersEngine
 
       def update(scraper_name, opts={})
         body = {}
-
         body[:name] = opts[:name] if opts[:name]
-        body[:git_repository] = opts[:repo] if opts[:repo]
-        body[:git_branch] = opts[:branch] if opts[:branch]
+        body[:git_repository] = opts[:repo] || opts[:git_repository] if opts[:repo] || opts[:git_repository]
+        body[:git_branch] = opts[:branch] || opts[:git_branch] if opts[:branch] || opts[:git_branch]
         body[:freshness_type] = opts[:freshness_type] if opts[:freshness_type]
-        body[:force_fetch] = opts[:force_fetch] if opts.has_key?("force_fetch")
-        body[:standard_worker_count] = opts[:workers] if opts[:workers]
-        body[:browser_worker_count] = opts[:browsers] if opts[:browsers]
+        body[:force_fetch] = opts[:force_fetch] if opts.has_key?("force_fetch") || opts.has_key?(:force_fetch)
+        body[:standard_worker_count] = opts[:workers] || opts[:standard_worker_count] if opts[:workers] || opts[:standard_worker_count]
+        body[:browser_worker_count] = opts[:browsers] || opts[:browser_worker_count] if opts[:browsers] || opts[:browser_worker_count]
         body[:proxy_type] = opts[:proxy_type] if opts[:proxy_type]
-        body[:disable_scheduler] = opts[:disable_scheduler] if opts.has_key?("disable_scheduler")
-        body[:cancel_current_job] = opts[:cancel_current_job] if opts.has_key?("cancel_current_job")
+        body[:disable_scheduler] = opts[:disable_scheduler] if opts.has_key?("disable_scheduler") || opts.has_key?(:disable_scheduler)
+        body[:cancel_current_job] = opts[:cancel_current_job] if opts.has_key?("cancel_current_job") || opts.has_key?(:cancel_current_job)
         body[:schedule] = opts[:schedule] if opts[:schedule]
         body[:timezone] = opts[:timezone] if opts[:timezone]
         @options.merge!({body: body.to_json})
